61f09716505793cbeae721ec2be6e21f4626606c
akua7 <ahmetkaygisiz17@yandex.com>
2016-10-19 09:16:05 +0300

    ikinci kayıt

---
+++ form1.php
@@ -0,0 +1,32 @@
+﻿<html> 
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<title> Form1 </title>
+	</head>
+
+	<body>
+		<form action = "http://localhost/web_programlama/form2.php" method ="GET"> 
+		
+			<br>Birinci : <input type = "text  " name = "birinci"><br>
+			<br>İkinci  : <input type = "text   " name ="ikinci"><br>
+			
+			<br> <input type = "submit" value = "Beşiktaş" >
+			<br>
+			
+			<a href="http://www.w3schools.com/html/default.asp">O ünlünün son hali görenleri şaşırttı.!!!!</a>
+			<table border="3" width=100%>
+		       <tr>
+			      <td>1</td>
+				  <td>2</td>
+				</tr>
+				<tr>
+			      <td>3</td>
+				  <td>4</td>
+				</tr>
+		</form>
+	</body>
+
+</html>
+
+
